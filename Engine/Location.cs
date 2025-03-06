f79302b2a9672adabbcfe8ce63e3d76cdd335149
agu3 <agulan822@gmail.com>
2025-03-06 19:44:41 +0200

    inheretance

--- Engine/Location.cs
+++ Engine/Location.cs
@@ -9,9 +9,35 @@ namespace Engine
 {
     public class Location
     {
-        public int ID { get; set; } 
+
+        public int ID { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
 
+        public Item ItemReqeustedToEnter { get; set; }
+        public Quest QuestAvailableHere { get; set; }
+        public Monster MonsterLivingHere { get; set; }
+        public Location LocatioToNorth { get; set; }
+        public Location LocatioToEast { get; set; }
+        public Location LocatioToSouth { get; set; }
+        public Location LocatioToWest { get; set; }
+        
+        public Location(int iD, string name, string description, Item itemReqeustedToEnter = null, Quest questAvailableHere = null,
+            Monster monsterLivingHere = null)
+        {
+            ID = iD;
+            Name = name;
+            Description = description;
+            ItemReqeustedToEnter = itemReqeustedToEnter;
+            QuestAvailableHere = questAvailableHere;
+            MonsterLivingHere = monsterLivingHere;
+        }
+
+        
+        
+
+
     }
+
+    
 }
--- SuperAdventure/SuperAdventure.cs
+++ SuperAdventure/SuperAdventure.cs
@@ -9,13 +9,14 @@ namespace SuperAdventure
         public SuperAdventure()
         {
             InitializeComponent();
-            _player = new Player();
+
+            Location location = new Location(1, "Home", "This is your house.");
+            location.ID = 1;
+            location.Name = "Home";
+            location.Description = "This is your home";
+
+            _player = new Player(10, 10, 20,0,1);
             {
-                _player.CurrentHitPoints = 10;
-                _player.MaximumHitPoints = 10;
-                _player.Gold = 20;
-                _player.ExperiencePoints = 0;
-                _player.Level = 1;
                 lblHitPoints.Text = _player.CurrentHitPoints.ToString();
                 lblGold.Text = _player.Gold.ToString();
                 lblExperience.Text = _player.ExperiencePoints.ToString();
@@ -29,6 +30,7 @@ namespace SuperAdventure
 
         }
 
+        
         
     }
 }
